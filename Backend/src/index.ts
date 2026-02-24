fedf397378fe669038843828fd3b77e6164f5cad
Tamojit Hazra <tamosrk@gmail.com>
2026-02-24 22:38:36 +0530

    initial commit

---
+++ Backend/src/index.ts
@@ -0,0 +1,88 @@
+import { WebSocketServer, WebSocket } from "ws";
+import { randomUUID } from "crypto";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface ExtendedWebSocket extends WebSocket {
+  clientId?: string;
+}
+
+interface User {
+  socket: WebSocket;
+  room: string;
+  clientId: string;
+}
+let allSocket: User[] = [];
+
+wss.on("connection", (socket: ExtendedWebSocket) => {
+  const clientId = randomUUID();
+  socket.clientId = clientId;
+
+  socket.send(
+    JSON.stringify({
+      type: "init",
+      clientId: clientId,
+    }),
+  );
+
+  //user will send
+  // join a room {
+  // "type": "join",
+  // "payload":{
+  //     "roomId": "123"
+  //             }
+  //     }
+  // send a message {
+  // "type": "chat",
+  // "payload":{
+  //     "message": "hi there"
+  //             }
+  //     }
+
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message as unknown as string);
+    if (parsedMessage.type === "join") {
+      console.log("user joined " + parsedMessage.payload.roomId);
+
+      allSocket.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+        clientId: parsedMessage.payload.clientId,
+      });
+    }
+
+    if (parsedMessage.type === "chat") {
+      const currentUser = allSocket.find((x) => x.socket == socket);
+      console.log("user sending " + parsedMessage.payload.message);
+
+      for (let i = 0; i < allSocket.length; i++) {
+        if (allSocket[i]?.room == currentUser?.room) {
+          allSocket[i]?.socket.send(
+            JSON.stringify({
+              type: "chat",
+              payload: {
+                message: parsedMessage.payload.message,
+                clientId: parsedMessage.payload.clientId,
+                roomId: currentUser?.room,
+              },
+            }),
+          );
+        }
+      }
+    }
+  });
+
+  console.log("user connected");
+
+  // socket.on("message", (message)=>{
+  //     console.log("message received " + message.toString());
+  //     // socket.send(message.toString() + ": sent from the server ")
+  //     allSocket.map((s)=>{
+  //         s.send(message.toString() + ": sent from the server ")
+  //     })
+  // })
+
+  socket.on("disconnect", () => {
+    allSocket = allSocket.filter((x) => x.socket != socket);
+  });
+});
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,201 @@
+import { useEffect, useRef, useState } from "react";
+
+interface ChatMessage {
+  roomId: string;
+  clientId: string;
+  message: string;
+}
+
+function App() {
+  const [myId, setMyId] = useState();
+  const [info, setInfo] = useState<ChatMessage[]>([]);
+  const [rooms, setRooms] = useState<Record<string, ChatMessage[]>>({});
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const roomInputRef = useRef<HTMLInputElement>(null);
+  const wsRef = useRef<WebSocket>(null);
+
+  function sendRoomId() {
+    const roomId = roomInputRef.current?.value;
+    if (wsRef.current) {
+      wsRef.current.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: roomId,
+            clientId: myId,
+          },
+        }),
+      );
+    }
+    if (roomInputRef.current) {
+      roomInputRef.current.value = "";
+    }
+  }
+
+  function Send() {
+    const message = inputRef.current?.value;
+    console.log("myId is " + myId);
+
+    if (wsRef.current) {
+      wsRef.current.send(
+        JSON.stringify({
+          type: "chat",
+          payload: {
+            message: message,
+            clientId: myId,
+          },
+        }),
+      );
+    }
+
+    if (inputRef.current) {
+      console.log(message);
+
+      inputRef.current.value = "";
+    }
+  }
+  useEffect(() => {
+    console.log("Updated info ", info);
+  }, [info]);
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  });
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+
+    ws.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+
+      if (data.type === "chat") {
+        const { roomId } = data.payload;
+
+        setRooms((prev) => ({
+          ...prev,
+          [roomId]: [...(prev[roomId] || []), data.payload],
+        }));
+
+        console.log("hi, i'm data", data.payload.message);
+        console.log("fking info ", info);
+      }
+
+      if (data.type === "init") {
+        setMyId(data.clientId);
+        console.log("init clientId " + data.clientId);
+
+        ws.send(
+          JSON.stringify({
+            type: "join",
+            payload: {
+              roomId: "1234",
+              clientId: data.clientId,
+            },
+          }),
+        );
+      }
+    };
+
+    wsRef.current = ws;
+
+    // ws.onopen = ()=>{
+    //   ws.send(JSON.stringify({
+    //     type:"join",
+    //     payload: {
+    //       roomId: "123",
+    //     }
+    //   }))
+    // }
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <div className=" flex  w-screen h-screen">
+      <div className=" w-full h-full bg-neutral-200 flex flex-col justify-between py-8 items-center rounded-xl">
+        <p className="text-2xl">Welcome</p>
+        <div className="flex flex-col overflow-y-scroll no-scrollbar  w-full h-full p-4 px-20">
+          {info.map((x) =>
+            x.clientId === myId ? (
+              <ChatTile type="right" message={x.message} />
+            ) : (
+              <ChatTile type="left" message={x.message} />
+            ),
+          )}
+          <div ref={messagesEndRef} />
+        </div>
+        <div className=" w-full">
+          <div className="flex gap-4 justify-between px-8 pt-4 items-center">
+            <input
+              ref={inputRef}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  Send();
+                }
+              }}
+              type="text"
+              placeholder="Type something"
+              className="outline-none p-4 w-full h-16 bg-white focus:ring-1 ring-blue-950  rounded-md"
+            />
+            <button
+              onClick={() => {
+                Send();
+              }}
+              className="bg-black w-20 text-white h-16 rounded-md"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen flex flex-col items-center py-8 bg-black text-white w-80">
+        <p className="text-xl">Sidebar</p>
+        <div className="flex gap-2 p-4">
+          <input
+            ref={roomInputRef}
+            type="text"
+            placeholder="Enter roomId"
+            className="bg-white text-black p-2 w-full flex rounded-md"
+          />
+          <button
+            onClick={() => {
+              sendRoomId();
+            }}
+            className="bg-white p-2 rounded-md text-black"
+          >
+            Join
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type ChatType = {
+  type?: "left" | "right";
+  message: string;
+};
+
+const ChatTile = ({ type, message }: ChatType) => {
+  const isRight = type === "right";
+
+  const alignment = isRight ? "flex-row-reverse" : "";
+  const bubbleStyle = isRight
+    ? "rounded-tr-sm rounded-b-2xl rounded-tl-2xl"
+    : "rounded-tl-sm rounded-b-2xl rounded-tr-2xl";
+
+  return (
+    <div className={`w-full flex py-4 px-2 ${alignment}`}>
+      <p
+        className={`bg-black text-white max-w-60 wrap-break-word p-4 ${bubbleStyle}`}
+      >
+        {message}
+      </p>
+    </div>
+  );
+};
+
+export default App;
